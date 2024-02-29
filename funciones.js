138ce8e9408d3b23e6063c70d2d146f7bd54a50d
juan1285 <147539441+juan1285@users.noreply.github.com>
2024-02-28 23:27:49 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,148 @@
+function saludar() {
+    const nombre = "UNID";
+    console.log("Hola", nombre);
+  }
+  
+  function saludarDos() {
+    const nombreDos = prompt("Ingresa un nombre");
+    console.log("Hola", nombreDos);
+  }
+ 
+
+    function saludartres(){
+      const nombretres = prompt("ingresa un nombre");
+      console.log("hola", nombretres)
+    }
+
+    
+    function saludarcuatro(){
+      const nombrecuatro = prompt("ingresa un nombre");
+      console.log("hola", nombrecuatro)
+    }
+
+    
+    function saludarcinco(){
+      const nombrecinco = prompt("ingresa un nombre");
+      console.log("hola", nombrecinco)
+    }
+
+    
+    function saludarseis(){
+      const nombreseis = prompt("ingresa un nombre");
+      console.log("hola", nombreseis)
+    }
+
+    
+    function saludarsiete(){
+      const nombresiete = prompt("ingresa un nombre");
+      console.log("hola", nombresiete)
+    }
+
+    
+    function saludarocho(){
+      const nombreocho = prompt("ingresa un nombre");
+      console.log("hola", nombreocho)
+    }
+
+    
+    function saludarnueve(){
+      const nombrenueve = prompt("ingresa un nombre");
+      console.log("hola", nombrenueve)
+    }
+
+    function saludardiez(){
+      const nombrediez = prompt("ingresa un nombre");
+      console.log("hola", nombrediez)
+    }
+    
+    function saludaronce(){
+      const nombreonce = prompt("ingresa un nombre");
+      console.log("hola", nombreonce)
+    }
+
+    
+    function saludardoce(){
+      const nombredoce = prompt("ingresa un nombre");
+      console.log("hola", nombredoce)
+    }
+
+    
+    function saludartrece(){
+      const nombretrece = prompt("ingresa un nombre");
+      console.log("hola", nombretrece)
+    }
+
+    
+    function saludarcatorce(){
+      const nombrecatorce = prompt("ingresa un nombre");
+      console.log("hola", nombrecatorce)
+    }
+
+    
+    function saludarquince(){
+      const nombrequince = prompt("ingresa un nombre");
+      console.log("hola", nombrequince)
+    }
+
+    
+    function saludardiciseis(){
+      const nombrediciseis = prompt("ingresa un nombre");
+      console.log("hola", nombrediciseis)
+    }
+
+    
+    function saludardicisiete(){
+      const nombredicisiete = prompt("ingresa un nombre");
+      console.log("hola", nombredicisiete)
+    }
+
+    
+    function saludardiciocho(){
+      const nombrediciocho = prompt("ingresa un nombre");
+      console.log("hola", nombrediciocho)
+    }
+
+    
+    function saludardicinueve(){
+      const nombredicinueve = prompt("ingresa un nombre");
+      console.log("hola", nombredicinueve)
+    }
+
+    
+    function saludarveinte(){
+      const nombreveinte = prompt("ingresa un nombre");
+      console.log("hola", nombreveinte)
+    }
+
+    
+    function saludarveintiuno(){
+      const nombreveintiuno = prompt("ingresa un nombre");
+      console.log("hola", nombreveintiuno)
+    }
+
+    
+    function saludarveintidos(){
+      const nombreveintidos = prompt("ingresa un nombre");
+      console.log("hola", nombreveintidos)
+    }
+
+    
+    function saludarveintitres(){
+      const nombreveintitres = prompt("ingresa un nombre");
+      console.log("hola", nombreveintitres)
+    }
+
+    
+    function saludarveinticuatro(){
+      const nombreveinticuatro = prompt("ingresa un nombre");
+      console.log("hola", nombreveinticuatro)
+    }
+
+    
+    function saludarveinticinco(){
+      const nombreveinticinco = prompt("ingresa un nombre");
+      console.log("hola", nombreveinticinco)
+    }
+
+    
+    
